e43b23001f2dadaac498074d300954bc86d2f8e6
Stephen Bunn <stephen@bunn.io>
2020-04-27 12:29:37 -0400

    Ignoring pep257 D213 as it violates D212 which is my default style
    Signed-off-by: Stephen Bunn <stephen@bunn.io>

--- tests/config/mod/strategies.py
+++ tests/config/mod/strategies.py
@@ -11,8 +11,7 @@ from modist.config.mod.mod import MOD_CONFIG_NAME_PATTERN, ModConfig
 
 @composite
 def mod_config_payload(draw, name_strategy=None) -> dict:
-    """
-    Composite strategy for building a mod config payload.
+    """Composite strategy for building a mod config payload.
 
     :param Callable[[SearchStrategy], Any] draw: The function for creating a strategy result
     :param SerachStrategy name_strategy: The strategy to use for building a mod name, defaults to None
@@ -31,8 +30,7 @@ def mod_config_payload(draw, name_strategy=None) -> dict:
 
 @composite
 def mod_config(draw, name_strategy=None) -> ModConfig:
-    """
-    Composite strategy for building a mod config instance.
+    """Composite strategy for building a mod config instance.
 
     :param Callabe[[SearchStrategy], Any] draw: The function for creating a strategy result
     :param name_strategy:  The strategy to use for building a mod name, defaults to None
